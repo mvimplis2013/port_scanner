2874ca45b567ecaf2a1c80f9f7dbe451083c7cf6
Miltos69 <mvimblis@gmail.com>
2018-09-06 00:14:16 +0300

    first message

--- robotnmap/RoboNmap.py
+++ robotnmap/RoboNmap.py
@@ -25,4 +25,50 @@ class RoboNmap(object):
         target = str(target)
 
         if file_export == None:
-            nmproc = NmapProcess(target, '-oN {0}'.format(nmproc.stderr))
+            nmproc = NmapProcess(target)
+        else:
+            nmproc = NmapProcess(target, '-oN {0}'.format(file_export), safe_mode=False)
+        
+        rc = nmproc.run()
+
+        if rc != 0:
+            raise Exception('EXCEPTION: nmap scan failed: {0}'.format(nmproc.stderr))
+        try:
+            parsed = NmapParser.parse(nmproc.stdout)
+            print parsed
+            self.results = parsed
+        except NmapParserException as ne:
+            print 'EXCEPTION: Exception in Parsing Results: {0}'.format(ne.msg)
+
+    def nmap_all_tcp_scan(self, target, file_export=None):
+        ...
+        Runs nmap scan against all TCP Ports with version  scanning. 
+        Options used are -Pn -sV -p1-65535
+
+        Examples:
+        nmap defa
+        ...
+        target = str(target)
+
+        if file_export == None:
+            nmproc = NmapProcess(target, '-p1-65535 -sV')
+        else:
+            cmd = '-p1-65535 -sV -oN {0}'.format(file_export)
+            nmproc = NmapProcess(target, cmd, safe_mode=False)
+
+        rc = nmproc.run()
+
+        if rc != 0:
+            raise Exception('EXCEPTION: nmap scan failed: {0}'.format(nmproc.stderr)
+        try:
+            parsed = NmapParser.parse(nmproc.stdout)
+            print parsed
+            self.results = parsed
+        except NmapParserException as ne:
+            print 'EXCEPTION: Exception in Parsing results: {0}'.format(ne.msg)
+
+    def nmap_specific_udp_scan(self, target, portlist, file_export = None):
+        ...
+        Runs nmap against specified UDP ports given in the portlist argument
+        ...
+         
