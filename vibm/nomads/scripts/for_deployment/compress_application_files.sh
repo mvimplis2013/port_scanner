ca47b391cf2f8b0fc3a894ac8d2ca33032104e16
student@e450-8 <student@e450-8.res.training>
2019-03-02 22:31:37 +0100

    robot-tgz

--- vibm/nomads/scripts/for_deployment/compress_application_files.sh
+++ vibm/nomads/scripts/for_deployment/compress_application_files.sh
@@ -1,12 +1,35 @@
+#!/bin/bash
+goodnames=("gui" "backend")
+appname=$1
+
+found=0
+for element in "${goodnames[@]}"; do 
+    if [[ $element == $appname ]]; then
+        found=1
+        break
+    fi
+done
+
+if [[ $found != 1 ]]; then
+    echo "Invalid Module Name ... ABORD"
+    exit 1
+fi
+
 tool="tar"
 
 flags="-cvzf" 
 
-tgz_name="archives/nomads_devel_source.tgz"
+tgz_name="archives/nomads_devel_source_"
+tgz_name="$tgz_name""$appname"".tgz"
+#echo "$tgz_name"
 
 src_folder="nomads/"
-test_folder="tests"
+src_folder="$src_folder""$appname""/"
+test_folder="tests/"
+test_folder="$test_folder""$appname""/"
+conf_folder="config/"
+conf_folder="$conf_folder""$appname""/"
 
 exclude_folder="__pycache__"
 
-$tool $flags $tgz_name $src_folder $test_folder --exclude=$exclude_folder
+$tool $flags $tgz_name $src_folder $test_folder $conf_folder --exclude=$exclude_folder
