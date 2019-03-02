504249328ab20a54c24ecdc24e0ec4c77749d672
student@e450-8 <student@e450-8.res.training>
2019-03-02 23:16:17 +0100

    test

--- vibm/nomads/scripts/for_user/run_backend_robot_microservices_from_source.sh
+++ vibm/nomads/scripts/for_user/run_backend_robot_microservices_from_source.sh
@@ -1,2 +1,2 @@
-wget https://raw.gihubusercontent/mvimplis2013/port_scanner/master/vibm/nomads/archives/nomads_devel_source_backend.tgz
+wget --no-check-certificate https://raw.gihubusercontent/mvimplis2013/port_scanner/master/vibm/nomads/archives/nomads_devel_source_backend.tgz
 tar -xvzf nomads_devel_source_backend.tgz
