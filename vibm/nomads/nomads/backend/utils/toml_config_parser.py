b0e911d2726a6e4f166192320b5d2c633990691d
student@e450-8 <student@e450-8.res.training>
2019-03-12 11:27:55 +0100

    database support

--- vibm/nomads/nomads/backend/utils/toml_config_parser.py
+++ vibm/nomads/nomads/backend/utils/toml_config_parser.py
@@ -49,7 +49,7 @@ class TomlParser(BaseParser):
 
     def get(self, section, option, default=NO_DEFAULT, **kwargs):
         try:
-            print("Data is ...\n%s" % str(data))
+            print("Data is ...\n%s" % str(self.data))
             return self.data[section][option]
         except KeyError:
             if default is self.NO_DEFAULT:
