d5e3358a1b515cea578753130dce26d558dafe52
student@e450-8 <student@e450-8.res.training>
2019-03-12 15:45:31 +0100

    database support

--- vibm/nomads/nomads/backend/datastore/ping_port_scan_table.py
+++ vibm/nomads/nomads/backend/datastore/ping_port_scan_table.py
@@ -17,7 +17,7 @@ class PingPortScanTable(object):
         
         self.table = Table( 'ping_port_scans', self.metadata, 
             Column('id', Integer(), primary_key=True),
-            Column('server_id', Integer(), ForeignKey('external_servers.id'))
+            Column('server_id', Integer(), ForeignKey('external_servers.id')),
             Column('port', Integer(), index=True),
             Column('protocol', String(3), CheckConstraint("protocol='tcp' OR protocol='udp'")),
             Column('state', String(5)),
