860c7154c5e23640bd74a35ea5a2172d103d2063
student@e450-8 <student@e450-8.res.training>
2019-03-12 10:45:50 +0100

    database support

--- vibm/nomads/nomads/backend/engine/back_robot.py
+++ vibm/nomads/nomads/backend/engine/back_robot.py
@@ -1,9 +1,18 @@
 from . import nomads_logger
-from . import Scheduler
+from . import TomlParser
 from . import DatabaseManager
+from . import Scheduler
 
 class BackRobot(object):
+    """
+    BackRobot operation depends on:
+     external configuration for actions frequencies, 
+     database tables for targets info and 
+     scheduler for actions timing. 
+    """
     def __init__(self):
+        self.toml_parser = TomlParser()
+        self.db_manager = DatabaseManager()
         self.scheduler = Scheduler
 
     """
@@ -16,7 +25,10 @@ class BackRobot(object):
     Watch external servers like a server that is not responding now ... waiting when will be alive again.
     """
     def watch_external_servers(self):
+        freq_mins = self.toml_parser.get("external-monitoring", "ip-ping-freq-mins")
+        nomads_logger.debug( "Frequency Minutes ... %d" int(freq_mins))
+
         self.scheduler.scheduleExternalServersPing()
-        
+
 
 
