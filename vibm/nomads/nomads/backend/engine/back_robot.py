58ea56e6c899a7df79f8d457b7eb5223672ecac3
student@e450-8 <student@e450-8.res.training>
2019-03-12 10:19:44 +0100

    database support

--- vibm/nomads/nomads/backend/engine/back_robot.py
+++ vibm/nomads/nomads/backend/engine/back_robot.py
@@ -1,5 +1,22 @@
 from . import nomads_logger
+from . import Scheduler
+from . import DatabaseManager
 
 class BackRobot(object):
     def __init__(self):
-        nomads_logger.debug("Back Robot is UP!")
+        self.scheduler = Scheduler
+
+    """
+    Starts all monitoring mechanisms external and internal.
+    """
+    def start_monitoring(self):
+        self.watch_external_servers()
+
+    """
+    Watch external servers like a server that is not responding now ... waiting when will be alive again.
+    """
+    def watch_external_servers(self):
+        self.scheduler.scheduleExternalServersPing()
+        
+
+
