c0d5552df49fd26434c1e03604c477fe0066e134
student@e450-8 <student@e450-8.res.training>
2019-03-14 10:10:36 +0100

    database support

--- vibm/nomads/nomads/backend/utils/nmap_ping_response.py
+++ vibm/nomads/nomads/backend/utils/nmap_ping_response.py
@@ -1,10 +1,15 @@
+_HOST_LINE = 2
+
 class NMapPingResponse(object):
     def __init__(self, response_text):
         self.response_text = response_text
 
     def is_host_up(self):
+        # Find the line with host status
         lines = self.response_text.split("\n")
-        return lines[3]
+        host_status_line = lines[ _HOST_LINE ].trim()
+
+        return host_status_line
 
 class NMapPingResponseWithPortsScan(NMapPingResponse):
     def __init__(self):
