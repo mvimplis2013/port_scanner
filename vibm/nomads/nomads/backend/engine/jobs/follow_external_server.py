791b7e2f3be9e31c9ac71275559dff81569acf8b
student@e450-8 <student@e450-8.res.training>
2019-03-14 10:02:07 +0100

    database support

--- vibm/nomads/nomads/backend/engine/jobs/follow_external_server.py
+++ vibm/nomads/nomads/backend/engine/jobs/follow_external_server.py
@@ -42,7 +42,7 @@ class FollowExternalServer(BaseJob):
             nomads_logger.debug( "*** Ping Reponse ***\n%s", ping_response)
 
             obj_repsonse = NMapPingResponse( ping_response )
-            nomads_logger.debug( obj_repsonse.is_server_running() )
+            nomads_logger.debug( obj_repsonse.is_host_up() )
 
     def stop(self):
         pass
