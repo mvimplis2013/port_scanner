1ae65f8d39d3c4db987b06967d07c2d0a2237203
student@e450-8 <student@e450-8.res.training>
2019-03-15 13:14:00 +0100

    database support

--- vibm/nomads/nomads/backend/utils/nmap_ping_response.py
+++ vibm/nomads/nomads/backend/utils/nmap_ping_response.py
@@ -4,14 +4,16 @@ from ..engine import nomads_logger
 
 _HOST_LINE = 2
 
+_OPEN_PORTS_FROM    = 5
+
 class NMapPingResponse(object):
     def __init__(self, response_text):
-        self.response_text = response_text
+        self.lines = response_text.split("\n")
+        
 
     def is_host_up(self):
         # Find the line with host status
-        lines = self.response_text.split("\n")
-        host_status_line = lines[ _HOST_LINE ].strip()
+        host_status_line = self.lines[ _HOST_LINE ].strip()
 
         _host_is_up = re.search("host is up", host_status_line, re.IGNORECASE)
 
@@ -22,9 +24,10 @@ class NMapPingResponse(object):
 
 class NMapPingResponseWithPortsScan(object):
     def __init__(self, response_text):
-        self.response_text = response_text
+        self.lines = response_text.split("\n")
 
     def get_open_ports_list(self):
-        nomads_logger.debug( "Inside NMapPing with Ports Scan Response ..." )
+        ports_header = self.lines[ _OPEN_PORTS_FROM ]
+        nomads_logger.debug( "Inside NMapPing with Ports Scan Response ... %s" % ports_header)
 
         return
