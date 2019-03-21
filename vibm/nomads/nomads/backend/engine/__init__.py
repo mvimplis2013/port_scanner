500c1923b12bb9748ee51764b99ca227ce648f71
student@e450-8 <student@e450-8.res.training>
2019-03-21 12:42:58 +0100

    charts support

--- vibm/nomads/nomads/backend/engine/__init__.py
+++ vibm/nomads/nomads/backend/engine/__init__.py
@@ -4,7 +4,6 @@ from ..utils.back_logger import nomads_logger
 #from ..config.external_monitoring_config_data import ExternalMonitoringConfigData
 from .test import External
 from .configurator import ExternalMonitoringConfigurator
-from .jobs.follow_external_server import FollowExternalServer
 
 from ..utils.nmap_native import NMapNative
 from ..utils.nmap_ping_response import NMapPingResponse, NMapPingResponseWithPortsScan
@@ -16,5 +15,7 @@ from ..datastore.database_manager import DatabaseManager
 from ..datastore.ping_port_scan_table import PingPortScanTable
 from ..datastore.external_servers_table import ExternalServersTable
 from ..datastore.ping_response_table import PingResponseTable
+
+from .jobs.follow_external_server import FollowExternalServer
 
 from .scheduler import Scheduler
