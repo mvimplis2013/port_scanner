2184ac941d7995193052813c5aaa75e5c81c8c6f
student@e450-8 <student@e450-8.res.training>
2019-03-22 11:32:59 +0100

    charts support

--- vibm/nomads/nomads/backend/datastore/ping_response_table.py
+++ vibm/nomads/nomads/backend/datastore/ping_response_table.py
@@ -116,8 +116,9 @@ class PingResponseTable(object):
             connection = engine.connect()
             nomads_logger.debug("Have a connection with db ... {}".format(connection_str))
             
-            select_stm = "SELECT server_id, dns_name, GROUP_CONCAT(ping_responses.is_up) GROUP_CONCAT(observation_datetime) FROM ping_responses, external_servers WHERE external_servers.id = server_id GROUP BY server_id"
+            #select_stm = "SELECT server_id, dns_name, GROUP_CONCAT(ping_responses.is_up) GROUP_CONCAT(observation_datetime) FROM ping_responses, external_servers WHERE external_servers.id = server_id GROUP BY server_id"
             #select_stm = "SELECT * FROM ping_responses"
+            select_stm = "SELECT server_id, GROUP_CONCAT(is_up) GROUP_CONCAT(observation_datetime) FROM ping_responses GROUP BY server_id"
             result = connection.execute( select_stm ) 
             
             for row in result:
