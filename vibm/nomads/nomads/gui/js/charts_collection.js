d740ec1e2d9f9c78b77b4e67e3a2a7f0901d6788
student@e450-8 <student@e450-8.res.training>
2019-03-24 22:58:13 +0100

    charts support

--- vibm/nomads/nomads/gui/js/charts_collection.js
+++ vibm/nomads/nomads/gui/js/charts_collection.js
@@ -17,7 +17,7 @@ function drawChart( server_id, _from, _to, _is_up ) {
     //    ["2017", 1030, 540, 350]
     //]);
 
-    var result = addMinutes(new Date(2014, 6, 10, 12, 0), 30);
+    var result = dateFns.addMinutes(new Date(2014, 6, 10, 12, 0), 30);
     console.log("New Date is ..." + result);
 
     var data = google.visualization.arrayToDataTable([
