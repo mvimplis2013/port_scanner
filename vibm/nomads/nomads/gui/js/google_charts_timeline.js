bd59ca829556e06a57e809f72fa259609b07ebea
student@e450-8 <student@e450-8.res.training>
2019-03-26 14:57:41 +0100

    google charts  support

--- vibm/nomads/nomads/gui/js/google_charts_timeline.js
+++ vibm/nomads/nomads/gui/js/google_charts_timeline.js
@@ -1,14 +1,16 @@
-function draw_timeline(server, a, b, is_up) {
+function draw_timeline( server, from_time, to_time, is_up ) {
     google.charts.load("current", {packages: ["timeline"]});
     google.charts.setOnLoadCallback(drawChart);
 
-    _server = server;
-    _from = a;
-    _to = b;
-    _is_up = is_up;
+    var _server     = server;
+    var _from       = from_time;
+    var _to         = to_time;
+    var _is_up      = is_up;
 
     function drawChart() {
-        console.log("Hello from Other Module ... " + _server);
+        console.log("Hello from Other Module ... " + _server + " / " + _from + " / " + _to );
+
+        var container = document.getElementById("timeline-container");
     }
 }
 
