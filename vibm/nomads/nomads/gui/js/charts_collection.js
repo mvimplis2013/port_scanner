685fd503742b6b99d6aba8e1badd2f74e7daad24
student@e450-8 <student@e450-8.res.training>
2019-03-26 10:04:07 +0100

    google charts support

--- vibm/nomads/nomads/gui/js/charts_collection.js
+++ vibm/nomads/nomads/gui/js/charts_collection.js
@@ -1,5 +1,3 @@
-import { draw_timeline } from "./google_charts.mjs";
-
 ROBOT_URL = "http://" + location.hostname + ":5000/"
 RPC_GET_VLAB_PING_DATA = "reports/external/get-ip-data"
 RPC_GET_VLAB_PORT_DATA = "reports/external/get-ip-and-port-data"
