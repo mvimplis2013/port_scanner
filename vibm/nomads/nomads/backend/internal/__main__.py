39d880c882f92aa03a02fc3d39995c6dbcc70309
student@e450-8 <student@e450-8.res.training>
2019-04-03 15:56:08 +0200

    internal-agent support

--- vibm/nomads/nomads/backend/internal/__main__.py
+++ vibm/nomads/nomads/backend/internal/__main__.py
@@ -23,7 +23,8 @@ for ip in all_ips:
 for mac in all_macs:
     print("MAC is ..." + mac)
 
-nmap = NMapNative( all_ips[0] + "/24" )
+#nmap = NMapNative( all_ips[0] + "/24" )
+nmap = NMapNative( "192.168.5.0/24" )
 nmap.run( INTERNAL_SCAN_MAC_ADDRESSES )
 
 print( "NMAP -sP -n response is ..." + nmap._result_utf8)
