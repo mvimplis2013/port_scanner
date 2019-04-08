1c2bb8b29273dc615ff81d3b1074dc2d5bb04470
student@e450-8 <student@e450-8.res.training>
2019-04-08 10:59:27 +0200

    mqtt-producer support

--- vibm/nomads/nomads/backend/internal/rabbitmq/run_rabbitmq.sh
+++ vibm/nomads/nomads/backend/internal/rabbitmq/run_rabbitmq.sh
@@ -1,12 +1,12 @@
 docker stop rabbit-server
 docker rm rabbit-server
 
-#docker rmi rabbitmq
-#docker build -t rabbitmq .
+docker rmi rabbitmq
 
 cd ~/projects/port_scanner
 git pull
 
 cd vibm/nomads/nomads/backend/internal/rabbitmq
 
+docker build -t rabbitmq .
 docker run -d --hostname my-rabbit --name rabbit-server --network app-tier rabbitmq
