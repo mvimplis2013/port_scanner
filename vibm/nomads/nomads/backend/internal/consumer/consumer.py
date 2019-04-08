0ae0baf8d07e7f7aabea17bbead5db7583cea096
student@e450-8 <student@e450-8.res.training>
2019-04-08 15:16:50 +0200

    mqtt-consumer support

--- vibm/nomads/nomads/backend/internal/consumer/consumer.py
+++ vibm/nomads/nomads/backend/internal/consumer/consumer.py
@@ -1,13 +1,48 @@
-import pika, logging
+import pika, logging, functools
 
-credentials = pika.PlainCredentials('nice_user', 'nice_user')
+def on_message(chan, method_frame, _header_frame, body,  userdata=None):
+    print("Userdata: " + userdata + " ... MessageBody: " + body)
+    chan.basic_ack(delivery_tag=method_frame.delivery_tag)
+    
+def main():
+    credentials = pika.PlainCredentials('nice_user', 'nice_user')
 
-parameters = pika.ConnectionParameters(host="rabbit-server", credentials=credentials)
+    parameters = pika.ConnectionParameters(host="rabbit-server", credentials=credentials)
 
-connection = pika.BlockingConnection( parameters )
+    connection = pika.BlockingConnection( parameters )
 
-channel = connection.channel()
+    channel = connection.channel()
 
-channel.
+    channel.exchange_declare(
+        exchange="test_exchange",
+        exchange_type='direct',
+        passive=False,
+        durable=True,
+        auto_delete=False
+    )
+
+    channel.queue_declare(queue='standard', auto_delete=False)
+
+    channel.queue_bind(
+        queue='standard', exchange='test_exchange', routing_key='standard_key'
+    )
+
+    channel.qos(prefetch_count=1)
+
+    on_message_callback = functools.partial(
+        on_message, userdata='on_message_userdata'
+    )
+
+    channel.basic_consume('standard', on_message_callback)
+
+    try: 
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+
+    connection.close()
+
+if __name__ == '__main__':
+    main()
 
 print("Consumer Planet")
