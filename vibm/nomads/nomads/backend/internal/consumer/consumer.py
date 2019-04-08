01338913766f1b6c5ab305de7cb0254edc639594
student@e450-8 <student@e450-8.res.training>
2019-04-08 14:52:30 +0200

    mqtt-consumer support

--- vibm/nomads/nomads/backend/internal/consumer/consumer.py
+++ vibm/nomads/nomads/backend/internal/consumer/consumer.py
@@ -1,6 +1,6 @@
 import pika, logging
 
-credentials = pika.PlainCredentials('guest', 'guest')
+credentials = pika.PlainCredentials('nice_user', 'nice_user')
 
 parameters = pika.ConnectionParameters(host="rabbit-server", credentials=credentials)
 
