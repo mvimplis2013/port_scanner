5656aabfe247c689caa62a4ac7bd67b7dd2ed5a8
student@e450-8 <student@e450-8.res.training>
2019-04-08 11:31:15 +0200

    rabbitmq support

---
+++ vibm/nomads/nomads/backend/internal/rabbitmq/init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+(sleep 5 ; \
+rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null; \
+rabbitmqctl set_user_tags $RABBITMQ_USER administrator; \
+rabbitmqctl set_permissions -p / $RABBITMQ_USER ".*" ".*" ".*"; \
+)
