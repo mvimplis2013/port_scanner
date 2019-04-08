cc1afb8a1940736c20a0206fa26cfb758a90d3be
student@e450-8 <student@e450-8.res.training>
2019-04-08 14:28:24 +0200

    mqtt support

--- vibm/nomads/nomads/backend/internal/consumer/consumer.py
+++ vibm/nomads/nomads/backend/internal/consumer/consumer.py
@@ -1,1 +1,9 @@
 import pika, logging
+
+connection = pika.BlockingConnection( 
+    pika.ConnectionParameters(host="my-rabbit")
+)
+
+main_channel = connection.channel()
+
+print("Consumer Planet")
---
+++ vibm/nomads/nomads/backend/internal/consumer/run_consumer.sh
@@ -0,0 +1,12 @@
+docker stop consumer-app
+docker rm consumer-app
+
+docker rmi consumer
+
+cd ~/projects/port-scanner
+git pull
+
+cd vibm/nomads/nomads/backend/internal/consumer
+
+docker build -t consumer .
+docker run --it --name consumer-app --network app-tier consumer /bin/ash
