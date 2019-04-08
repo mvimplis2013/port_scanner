1595979daedd4c7bdc0cd65b74444940b6c05843
student@e450-8 <student@e450-8.res.training>
2019-04-08 14:32:28 +0200

    mqtt support

--- vibm/nomads/nomads/backend/internal/consumer/run_consumer.sh
+++ vibm/nomads/nomads/backend/internal/consumer/run_consumer.sh
@@ -9,4 +9,4 @@ git pull
 cd vibm/nomads/nomads/backend/internal/consumer
 
 docker build -t consumer .
-docker run --it --name consumer-app --network app-tier consumer /bin/ash
+docker run -it --name consumer-app --network app-tier consumer /bin/ash
