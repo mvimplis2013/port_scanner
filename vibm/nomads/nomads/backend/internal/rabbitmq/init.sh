e6f44ff09ea52bb33b30e5e30c3a38438876ccda
student@e450-8 <student@e450-8.res.training>
2019-04-08 12:01:05 +0200

    rabbitmq support

--- vibm/nomads/nomads/backend/internal/rabbitmq/init.sh
+++ vibm/nomads/nomads/backend/internal/rabbitmq/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 (sleep 5 ; \
-rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null; \
-rabbitmqctl set_user_tags $RABBITMQ_USER administrator; \
-rabbitmqctl set_permissions -p / $RABBITMQ_USER ".*" ".*" ".*"; \
+rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
+#rabbitmqctl set_permissions -p / $RABBITMQ_USER ".*" ".*" ".*" ; \
 ) &
 
 # Call original entrypoint 
