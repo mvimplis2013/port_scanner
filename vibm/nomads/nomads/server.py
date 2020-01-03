f6cbd73018506c149c0b2276783714fdfa7acb87
mvimplis2013 <mvimblis@gmail.com>
2020-01-03 14:32:00 +0200

    server strips special characters from iot_records like ''

--- vibm/nomads/nomads/server.py
+++ vibm/nomads/nomads/server.py
@@ -248,9 +248,9 @@ def check_iot_data_availability():
     # num_iot_records = len(iot_data_records)
 
     # Remove special symbols like '' in '2019-12-0'
-    iot_record_0 = iot_data_records[0]
+    iot_record_0 = str(iot_data_records[0])
     #.replace('"', '') 
-    iot_record_1 = iot_data_records[1]
+    iot_record_1 = str(iot_data_records[1])
     #.replace('"', '') 
     
     #return f"<h1>Number of IoT Records Found in Database = {num_iot_records}</h1>"
