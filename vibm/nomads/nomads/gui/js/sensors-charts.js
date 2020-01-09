e073678a778f2ffb6834a9963fb485f08d2aba5f
mvimplis2013 <mvimblis@gmail.com>
2020-01-09 21:26:29 +0200

    change ALERT with CONFIRM

--- vibm/nomads/nomads/gui/js/sensors-charts.js
+++ vibm/nomads/nomads/gui/js/sensors-charts.js
@@ -4,7 +4,7 @@ document.addEventListener("contextmenu", function(e) {
 
 document.getElementById("sensor-1-status").onmousedown = function(event) {
     if (event.which == 3) {
-        alert("Right Clicked");
         event.preventDefault();
+        confirm("DO you really want to Toggle Sensor State ?");
     }
 }
