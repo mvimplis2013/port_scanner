ca6f97c50cbd948d43b59bc8edd9f1faa3567dea
mvimplis2013 <mvimblis@gmail.com>
2020-01-09 21:41:21 +0200

    Use AJAX to call serverside python-flask from JS

--- vibm/nomads/nomads/gui/js/sensors-charts.js
+++ vibm/nomads/nomads/gui/js/sensors-charts.js
@@ -5,6 +5,23 @@ document.addEventListener("contextmenu", function(e) {
 document.getElementById("sensor-1-status").onmousedown = function(event) {
     if (event.which == 3) {
         event.preventDefault();
-        confirm("DO you really want to Toggle Sensor State ?");
+        
+        if (confirm("Do you really want to Toggle Sensor State ?")) {
+            // Proceed with Sensor State Toggle
+            $.ajax({
+                type: "POST",
+                url: "mqtt/publish_state",
+                dataType: "json",
+                data: JSON.stringnify("lamp"),
+                contentType: "application/json;charset=UTF-8",
+                success: function(data) {
+                    console.log(data);
+                }
+            })
+        }
+        else {
+            // Do nothing !
+            conmsole.log("Do Nothing with Toggle State")
+        }
     }
 }
