43621a02c7f96400dbfbe1cb19ac7c388ebf265e
mvimplis2013 <mvimblis@gmail.com>
2020-01-09 21:19:44 +0200

    prevent default behavior in Right-Click

--- vibm/nomads/nomads/gui/js/sensors-charts.js
+++ vibm/nomads/nomads/gui/js/sensors-charts.js
@@ -1,3 +1,7 @@
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+}, false);
+
 document.getElementById("sensor-1-status").onmousedown = function(event) {
     if (event.which == 3) {
         alert("Right Clicked");
